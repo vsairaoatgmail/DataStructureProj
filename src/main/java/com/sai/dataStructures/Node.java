a34f056e08e283291f6b2511c874709ae3094510
Sai <vsairao@gmail.com>
2022-10-20 07:41:28 -0500

    commit

---
+++ src/main/java/com/sai/dataStructures/Node.java
@@ -0,0 +1,24 @@
+package com.sai.dataStructures;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+
+}
